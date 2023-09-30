ebc76cc3fb2800a44ea33be0ee7eba95986e5cad
daniboy1995 <132506484+daniboy1995@users.noreply.github.com>
2023-09-29 22:18:11 -0300

    Update codigomapa.py

--- codigomapa.py
+++ codigomapa.py
@@ -1,30 +1,22 @@
 import pandas as pd
 import folium
 from folium import PolyLine, Marker
-import ipywidgets as widgets
-from IPython.display import display
 import streamlit as st
 
-
+# Carregue os dados
 url = "https://raw.githubusercontent.com/daniboy1995/mapaod/main/dadosmapa.csv"
 df = pd.read_csv(url)
 
-# Crie uma função para atualizar as opções do filtro de rota com base na seleção de zona de tráfego
-def atualizar_rotas(change, default_value=(1,)):
-    if not change.new:
-        change.new = default_value
-    filtro_route.options = df[df['trafficzoneidorign'].isin(change.new)]['route'].unique()
-
-# Crie uma função para plotar o mapa com base nas seleções feitas
+# Função para filtrar e plotar o mapa
 def plotar_mapa(zonas_selecionadas, ticketdate_selecionada, route_selecionada):
     # Filtra o DataFrame com base nas seleções
     df_filtrado = df[df['trafficzoneidorign'].isin(zonas_selecionadas) &
                      (df['ticketdate'] == ticketdate_selecionada) &
                      (df['route'] == route_selecionada)]
 
-    # Verifique se há pelo menos uma linha válida no DataFrame filtrado
+    # Verifica se há pelo menos uma linha válida no DataFrame filtrado
     if not df_filtrado.empty:
-        # Crie um mapa centralizado nas coordenadas médias das origens
+        # Cria um mapa centralizado nas coordenadas médias das origens
         mapa = folium.Map(location=[df_filtrado['latorigem'].mean(), df_filtrado['lonorigem'].mean()], zoom_start=10)
 
         for index, row in df_filtrado.iterrows():
@@ -33,51 +25,39 @@ def plotar_mapa(zonas_selecionadas, ticketdate_selecionada, route_selecionada):
             rota = row['route']
             zona = row['trafficzoneidorign']
             
-            # Obtenha a contagem de usercards para esta rota
+            # Obtém a contagem de usercards para esta rota
             usercards_count = df_filtrado[df_filtrado['route'] == rota]['usercard'].count()
             
-            # Crie um pop-up com informações sobre a rota e o número de usercards
+            # Cria um pop-up com informações sobre a rota e o número de usercards
             popup_text = f"Origem: {origem}<br>Destino: {destino}<br>Rota: {rota}<br>Usercards: {usercards_count}"
             
-            # Defina a cor do marcador da origem para ser diferente da cor do destino
+            # Define a cor do marcador da origem para ser diferente da cor do destino
             cor = 'green' if origem == (row['latorigem'], row['lonorigem']) else 'red'
             
-            # Adicione marcadores de origem e destino com a cor apropriada
+            # Adiciona marcadores de origem e destino com a cor apropriada
             folium.Marker(location=origem, icon=folium.Icon(color=cor), tooltip=popup_text).add_to(mapa)
             folium.Marker(location=destino, icon=folium.Icon(color='blue'), tooltip=popup_text).add_to(mapa)
 
             linha = PolyLine(locations=[origem, destino], color='blue', tooltip=popup_text)
             linha.add_to(mapa)
 
-        display(mapa)
+        # Exibe o mapa no Streamlit
+        st.folium_chart(mapa)
     else:
-        print("Nenhum dado encontrado para as seleções feitas.")
+        st.write("Nenhum dado encontrado para as seleções feitas.")
 
-# Crie um widget de seleção múltipla para a zona de tráfego de origem
+# Widget para seleção de zona de tráfego
 zonas_de_trafego = df['trafficzoneidorign'].unique()
-filtro_zona_trafego = widgets.SelectMultiple(
-    options=zonas_de_trafego,
-    description='Zona de Tráfego:',
-    disabled=False,
-    value=(1,)  # Defina o valor padrão como (1,)
-)
+zonas_selecionadas = st.multiselect("Selecione as Zonas de Tráfego:", zonas_de_trafego, default=(1,))
 
-# Crie widgets de seleção para o ticketdate e a rota
+# Widget para seleção de data do ticket
 ticketdates = df['ticketdate'].unique()
-filtro_ticketdate = widgets.Dropdown(
-    options=ticketdates,
-    description='Ticket Date:',
-    disabled=False,
-)
-filtro_route = widgets.Dropdown(
-    options=df['route'].unique(),
-    description='Route:',
-    disabled=False,
-)
+ticketdate_selecionada = st.selectbox("Selecione a Data do Ticket:", ticketdates)
 
-# Defina um observador para atualizar as opções do filtro de rota com base na seleção de zona de tráfego
-filtro_zona_trafego.observe(atualizar_rotas, 'value')
+# Widget para seleção de rota
+routes = df['route'].unique()
+route_selecionada = st.selectbox("Selecione a Rota:", routes)
 
-# Exiba os widgets de seleção
-widgets.interactive(plotar_mapa, zonas_selecionadas=filtro_zona_trafego, 
-                    ticketdate_selecionada=filtro_ticketdate, route_selecionada=filtro_route)
+# Botão para plotar o mapa
+if st.button("Plotar Mapa"):
+    plotar_mapa(zonas_selecionadas, ticketdate_selecionada, route_selecionada)
